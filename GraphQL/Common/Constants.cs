9e322ae7ad13a9c49eda0958dee06b64a24f1aa6
Randel Ramirez <randelramirez1@gmail.com>
2021-05-08 22:19:51 +0800

    Added Common

---
+++ GraphQL/Common/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace GraphQL.Common
+{
+    public static class Constants
+    {
+        public const string QUERY = "Query";
+
+        public const string MUTATION = "Mutation";
+
+        public const string SUBSCRIPTION = "Subscription";
+    }
+}
