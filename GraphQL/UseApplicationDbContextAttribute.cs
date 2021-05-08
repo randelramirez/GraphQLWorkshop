b4725c5eba2c25bd237db761c871c80c94cafb91
Randel Ramirez <randelramirez1@gmail.com>
2021-05-08 22:29:53 +0800

    Add UseApplicationDbContextAttribute

---
+++ GraphQL/UseApplicationDbContextAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using GraphQL.Data;
+using GraphQL.Extensions;
+using HotChocolate.Types;
+using HotChocolate.Types.Descriptors;
+
+namespace GraphQL
+{
+    public class UseApplicationDbContextAttribute : ObjectFieldDescriptorAttribute
+    {
+        public override void OnConfigure(
+            IDescriptorContext context,
+            IObjectFieldDescriptor descriptor,
+            MemberInfo member)
+        {
+            descriptor.UseDbContext<DataContext>();
+        }
+    }
+}
